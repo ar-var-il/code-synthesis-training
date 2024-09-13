87a84916d0a7f333b09d3ac1a1404e6c31600dcc
Arturo Vargas <arturo.vargas@imaginelearning.com>
2024-09-13 12:56:52 -0600

    Add initial setup from spring initilzer

---
+++ src/main/java/com/encora/code_synthesis_training/CodeSynthesisTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.encora.code_synthesis_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSynthesisTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSynthesisTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encora/code_synthesis_training/CodeSynthesisTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.code_synthesis_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSynthesisTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
